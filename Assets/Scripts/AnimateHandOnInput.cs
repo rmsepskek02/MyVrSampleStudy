c00bac3bb26e4f5c9ef0197fbe4a546960374907
rmsepskek02 <rmsepskek02@gmail.com>
2024-11-13 14:52:13 +0900

    1113 강의
    1. unity 6 버전으로 컨버팅
    2. 이동, 회전, 텔레포트 기능 구현

---
+++ Assets/Scripts/AnimateHandOnInput.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace MyVrSample
+{
+    public class AnimateHandOnInput : MonoBehaviour
+    {
+        #region Variables
+        private Animator handAnimator;
+
+        //인풋 입력값 처리
+        public InputActionProperty pinchAnimationAction;
+        public InputActionProperty gripAnimationAction;
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            //참조
+            handAnimator = GetComponent<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+            float gripValue = gripAnimationAction.action.ReadValue<float>();
+            //Debug.Log($"triggerValue: {triggerValue}");
+
+            handAnimator.SetFloat("Trigger", triggerValue);
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+    }
+}
